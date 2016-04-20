f7751488e6ce74b88669f72277b019ea74759c6f
val kharitonashvili <valeri.kharitonashvili1@gmail.com>
2016-04-20 17:51:38 +0400

    something

--- includes/header.php
+++ includes/header.php
@@ -7,13 +7,25 @@
     <meta charset="utf-8">
 
     <!--styles-->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/font-awesome.min.css">
-    <link rel="stylesheet" href="../css/fonts.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <!--scripts-->
-    <script type="text/javascript" src="../js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
 </head>
 <body>
 
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img alt="Brand" src="...">
+                </a>
+            </div>
+        </div>
+    </div>
+
+</nav>
