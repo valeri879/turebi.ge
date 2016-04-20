36053bbf70bc536cf163b4a864e3d982a30e66c1
val kharitonashvili <valeri.kharitonashvili1@gmail.com>
2016-04-20 11:29:21 +0400

    folders

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Turebi.ge</title>
+    <meta charset="utf-8">
+
+    <!--styles-->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+<body>
+
